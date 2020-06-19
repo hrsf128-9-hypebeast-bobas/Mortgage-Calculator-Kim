e1bb89cb4c65364b3c9ef00b5985b2ad54941a3f
Kim Dang <kimdang229@gmail.com>
2020-06-19 16:38:09 -0700

    Centered container

--- client/src/Components/PreQualified.jsx
+++ client/src/Components/PreQualified.jsx
@@ -6,10 +6,8 @@ const PreQualified = () => {
     <div className="pre-qualified-container">
       <div className="payment-cell center-cell">
         <div className="payment-content">
-          <div style={{ width: '50%' }}>
-            <div className="btn">
-              <button type="button">Get Pre-Qualified</button>
-            </div>
+          <div className="qualified-center">
+            <button type="button" className="pre-qualified-btn">Get Pre-Qualified</button>
           </div>
         </div>
       </div>
@@ -17,7 +15,7 @@ const PreQualified = () => {
       <div className="payment-container seperate-cols">
         <div className="payment-cell center-cell">
           <div className="payment-content">
-            <div style={{ width: '50%' }}>
+            <div className="qualified-center">
               <div className="other-text">
                 or
               </div>
@@ -28,11 +26,9 @@ const PreQualified = () => {
       {/* **************** SEE TODAY'S MORTGAGE RATES **************** */}
       <div className="payment-container seperate-cols">
         <div className="payment-cell center-cell">
-          <div className="payment-content">
-            <div style={{ width: '50%' }}>
-              <div className="other-text" style={{ color: 'teal' }}>
-                See today's mortgage rates
-              </div>
+          <div className="qualified-center">
+            <div className="other-text" style={{ color: 'teal' }}>
+              See today's mortgage rates
             </div>
           </div>
         </div>
