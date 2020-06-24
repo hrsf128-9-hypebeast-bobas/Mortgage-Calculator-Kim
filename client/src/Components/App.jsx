7354174323fd2db290bdc66567129732f2617aef
Kim Dang <kimdang229@gmail.com>
2020-06-24 12:12:21 -0700

    Remove style.css

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import regeneratorRuntime from 'regenerator-runtime';
 import styles from '../styles/App.css';
 
